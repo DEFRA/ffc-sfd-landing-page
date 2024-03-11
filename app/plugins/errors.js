745c0a58872e9fbfe5dba97800aa955d80e15d57
John Watson <john.watson1@defra.gov.uk>
2024-03-11 16:43:41 +0000

    Remove log

--- app/plugins/errors.js
+++ app/plugins/errors.js
@@ -11,7 +11,6 @@ module.exports = {
           const statusCode = response.output.statusCode
 
           if (statusCode === 401) {
-            console.log(`Redirecting to ${serverConfig.gatewayHost}/auth/sign-in?redirect=${request.url.pathname}`)
             return h.redirect(`${serverConfig.gatewayHost}/auth/sign-in?redirect=${request.url.pathname}`)
           }
 

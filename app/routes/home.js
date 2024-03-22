fb1a800d4f9817b3c01e42d5c795bdce5812e47a
John Watson <john.watson1@defra.gov.uk>
2024-03-22 22:07:01 +0000

    Update landing page

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,33 +1,71 @@
 const Wreck = require('@hapi/wreck')
 const { GET } = require('../constants/http-verbs')
-const { USER } = require('ffc-auth/scopes')
+const { SFD_VIEW } = require('ffc-auth/scopes')
 const { serverConfig } = require('../config')
 const { AUTH_COOKIE_NAME } = require('../constants/cookies')
 
 module.exports = [{
   method: GET,
   path: '/home',
-  options: { auth: { strategy: 'jwt', scope: [USER] } },
+  options: { auth: { strategy: 'jwt', scope: [SFD_VIEW] } },
   handler: async (request, h) => {
-    const query = `query {
-          personOrganisations {
-            crn
-            organisations {
-              id
-              sbi
-              name
-            }
-          }
-        }`
-    const { payload } = await Wreck.post(serverConfig.dataHost, {
-      headers: {
-        crn: request.auth.credentials.crn,
-        Authorization: request.state[AUTH_COOKIE_NAME],
-        'Content-Type': 'application/json'
-      },
-      payload: JSON.stringify({ query }),
-      json: true
-    })
-    return h.view('home', { organisations: payload.data.personOrganisations.organisations, ahwpHost: serverConfig.ahwpHost })
+    const person = await getPerson(request)
+    const organisation = await getOrganisation(request)
+    return h.view('home', { person, organisation, ahwpHost: serverConfig.ahwpHost })
   }
 }]
+
+const getPerson = async (request) => {
+  const query = `query {
+          person {
+            crn
+            title
+            firstName
+            lastName
+            landline
+            mobile
+            email
+            address: {
+              fullAddress
+            }
+            doNotContact
+            locked
+          }
+        }`
+  const { payload } = await Wreck.post(serverConfig.dataHost, {
+    headers: {
+      crn: request.auth.credentials.crn,
+      Authorization: request.state[AUTH_COOKIE_NAME],
+      'Content-Type': 'application/json'
+    },
+    payload: JSON.stringify({ query }),
+    json: true
+  })
+  return payload.data.person
+}
+
+const getOrganisation = async (request) => {
+  const query = `query {
+          organisation(organisationId: "${request.auth.credentials.organisationId}") {
+            sbi
+            name
+            mobile
+            email
+            address {
+              fullAddress
+            }
+            type
+            legalStatus
+          }
+        }`
+  const { payload } = await Wreck.post(serverConfig.dataHost, {
+    headers: {
+      crn: request.auth.credentials.crn,
+      Authorization: request.state[AUTH_COOKIE_NAME],
+      'Content-Type': 'application/json'
+    },
+    payload: JSON.stringify({ query }),
+    json: true
+  })
+  return payload.data.organisation
+}
